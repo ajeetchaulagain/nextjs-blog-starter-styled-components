e7447a52ed9a22b51147f76149b8748d0aa2249d
ajeetchaulgain <chaulagainajeet@gmail.com>
2023-12-27 12:43:38 +1100

    feat: style Date component time element

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,10 +1,23 @@
+"use client";
 import { parseISO, format } from "date-fns";
+import styled from "styled-components";
 
 type DateProps = {
   dateString: string;
 };
 
+const StyledTime = styled.time`
+  color: ${({ theme }) => theme.colors.purpleHeart};
+  font-size: 0.9rem;
+  text-transform: uppercase;
+  font-weight: 500;
+`;
+
 export const Date = ({ dateString }: DateProps) => {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  return (
+    <StyledTime dateTime={dateString}>
+      {format(date, "LLLL d, yyyy")}
+    </StyledTime>
+  );
 };
